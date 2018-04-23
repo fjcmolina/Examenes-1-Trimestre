cdcb5fdac3f25d9cf2d6667b3de418724ddf05ca
fjcmolina <fjcmolina7@gmail.com>
2018-04-23 11:40:01 +0200

    Actualizando

--- 2013_2014_t1c1b/Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ 2013_2014_t1c1b/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -1,9 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *   Escribe un programa que mezcle los dígitos de dos números de la misma longitud que se introducen
+ *   por teclado. Se debe comprobar que los dos números efectivamente tienen la misma longitud. Se
+ *   recomienda usar long en lugar de int ya que el primero admite números más largos.
+ *
+ *   Ejemplo1:
+ *   Por favor, introduzca un número entero positivo: 54
+ *   Ahora introduzca otro número de la misma longitud que el anterior: 321
+ *   Los datos introducidos no son válidos, los dos números deben tener la misma longitud.
+ *
+ *   Ejemplo2:
+ *   Por favor, introduzca un número entero positivo: 543
+ *   Ahora introduzca otro número de la misma longitud que el anterior: 321
+ *   La mezcla de los dos números es 534231.
  */
 package ejercicio2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +23,15 @@ package ejercicio2;
  */
 public class Ejercicio2 {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long numero1 = s.nextInt();
+    
+    System.out.print("Ahora introduzca otro número de la misma longitud que el anterior: ");
+    long numero2 = s.nextInt();
   }
   
 }
