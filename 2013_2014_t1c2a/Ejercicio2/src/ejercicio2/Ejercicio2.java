07d0feb145dda890c8cd8ff064e328d661fbf02f
fjcmolina <fjcmolina7@gmail.com>
2018-04-24 10:16:43 +0200

    Actualizando

---
+++ 2013_2014_t1c2a/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,65 @@
+/*
+ *   Sinestesio y Casilda van a pintar los tres dormitorios de su casa, quieren sustituir el color blanco por
+ *   colores más alegres. Realiza un programa que genere de forma aleatoria una secuencia de tres
+ *   colores aleatorios (uno para cada dormitorio) de tal forma que no se repita ninguno. Los colores entre
+ *   los que debe elegir el programa son los siguientes: rojo, azul, verde, amarillo, violeta y naranja.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio2 {
+
+  public static void main(String[] args) {
+
+    final int numHabitaciones = 3;
+    
+    String color1 = "";
+    String color2 = "";
+    String color3 = "";
+    String color = "";
+    
+    int numColor = 0;
+    int i = 0;
+    
+    while (i < numHabitaciones){
+      numColor = (int)(Math.random()*5);
+      switch (numColor){
+        case 0:
+          color = "rojo";
+          break;
+        case 1:
+          color = "azul";
+          break;
+        case 2:
+          color = "verde";
+          break;
+        case 3:
+          color = "amarillo";
+          break;
+        case 4:
+          color = "violeta";
+          break;
+        default:
+          color = "naranja";
+      }
+      if (i == 0){
+        color1 = color;
+        i++;
+      } else if ((i == 1) && (color1 != color)){
+        color2 = color;
+        i++;
+      } else if ((i == 2) && (color1 != color) && (color2 != color)){
+        color3 = color;
+        i++;
+      }
+    }
+    System.out.println("Color de la primera habitación: " + color1);
+    System.out.println("Color de la segunda habitación: " + color2);
+    System.out.println("Color de la tercera habitación: " + color3);
+
+  }
+
+}
