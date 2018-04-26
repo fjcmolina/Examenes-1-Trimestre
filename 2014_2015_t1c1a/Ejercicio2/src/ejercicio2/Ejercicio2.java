b0a2bc117cc92a7f7a361011c87830f3d04d69e2
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 10:33:31 +0200

    Actualizando

---
+++ 2014_2015_t1c1a/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,34 @@
+/*
+ *     Escribe un programa que pida un número entero positivo y que diga a continuación si las cifras que lo
+ *     componen están ordenadas de menor a mayor, mirando de izquierda a derecha. Se podrán introducir
+ *     números de cualquier longitud mientras lo permitan las variables. Usa long en lugar de int donde sea
+ *     necesario para admitir números largos. Fíjate bien que no se pide ordenar las cifras, solo saber si
+ *     están ordenadas o no.
+ *
+ *     Ejemplo 1:
+ *     Por favor, introduzca un número entero positivo: 34667
+ *     Los dígitos del número 34667 están ordenados.
+ *
+ *     Ejemplo 2:
+ *     Por favor, introduzca un número entero positivo: 85
+ *     Los dígitos del número 85 no están ordenados.
+ */
+package ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio2 {
+
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("Por favor, introduzca un número entero positivo:");
+    long numero =  s.nextLong();
+  }
+  
+}
