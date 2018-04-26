921fd860902cef423b0bf590ef3e644391dd71a8
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 11:06:21 +0200

    Actualizando

---
+++ 2014_2015_t1c1b/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,23 @@
+/*
+ *     Escribe un programa que calcule el sueldo bruto a percibir por un trabajador de la construcción en un
+ *     mes. El programa debe pedir el cargo desempeñado (un número del 1 al 3) y el número de horas
+ *     trabajadas. Los cargos existentes son peón, albañil y encargado de obra; con un sueldo base de 1000,
+ *     1400 y 1700 euros al mes respectivamente. El sueldo base se cobra trabajando exactamente 140
+ *     horas al mes. Si un empleado trabaja menos de 140 horas en un mes, cobra la parte proporcional del
+ *     sueldo. Si trabaja más de 140 horas, cada hora extra se paga al 2% del salario base, es decir a 20, 28
+ *     o 34 euros según el cargo. Por ejemplo, un albañil que haya trabajado 150 horas ganaría 1680 euros
+ *     (1400 + 280). Un encargado de obra que haya trabajado 70 horas ganaría 850 euros.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
