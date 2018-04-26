18a6d249ac52f55fd4fd0f0b7f7a5a6a88b1b09b
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 09:46:33 +0200

    Actualizando

--- 2013_2014_t1rec/Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ 2013_2014_t1rec/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -16,13 +16,23 @@ import java.util.Scanner;
 public class Ejercicio2 {
 
   public static void main(String[] args) {
-    
+
     Scanner s = new Scanner(System.in);
-    
+
     System.out.println("Por favor, introduzca un número entero positivo: ");
-    int numero = s.nextInt();
-    
-  
+    int numeroIntroducido = s.nextInt();
+
+    int numero = numeroIntroducido;
+    int longitud = 0;
+    do {
+      numero /= 10;
+      longitud++;
+
+    } while (numero > 0);
+
+    int digito = (int) (Math.random() * longitud) + 1;
+
+    System.out.println((numeroIntroducido / (int) (Math.pow(10, longitud - digito))) % 10);
   }
-  
+
 }
