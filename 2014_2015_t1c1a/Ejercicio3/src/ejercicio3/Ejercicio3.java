7a07e4617ded5c1ac23625cb268532cabfe57ff0
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 10:38:22 +0200

    Actualizando

--- 2014_2015_t1c1a/Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ 2014_2015_t1c1a/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *   Realiza un programa que pida 10 números enteros por teclado y que luego diga cuántos de esos
+ *   números son capicúa. Los números capicúa son los que se leen igual hacia delante y hacia atrás. Los
+ *   capicúa repetidos también cuentan.
  */
 package ejercicio3;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +13,28 @@ package ejercicio3;
  */
 public class Ejercicio3 {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+
+    Scanner s = new Scanner(System.in);
+
+    for (int i = 0; i < 10; i++) {
+      System.out.print("\nIntroduce un numero: ");
+      long numeroIntroducido = s.nextLong();
+
+      long numero = numeroIntroducido;
+      long numeroReves = 0;
+
+      while (numero > 0) {
+        numeroReves = (numeroReves * 10) + (numero % 10);
+        numero /= 10;
+      }
+
+      if (numeroIntroducido == numeroReves) {
+        System.out.println("El numero es capicua ");
+      } else {
+        System.out.println("El numero no es capicua");
+      }
+    }
   }
-  
+
 }
