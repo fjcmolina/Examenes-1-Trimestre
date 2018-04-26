103ad25d5f27ac2987575aca82e7cdffa7c296f7
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 08:25:32 +0200

    Actualizando

---
+++ 2013_2014_t1rec/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,34 @@
+/*
+ *     Realiza un programa que genere un tique de compra a partir del precio inicial y del código de
+ *     descuento. Se aplicará siempre un IVA del 21%. El precio inicial se debe indicar en euros y puede
+ *     tener decimales. El descuento se aplica al precio inicial, antes de calcular el importe del IVA. Los
+ *     códigos de descuento son “FCJ4”, “Q25Y” y “AM68” que restan 3, 5 y 10 euros respectivamente
+ *     (siempre que el importe de la compra sea igual o inferior al descuento que se quiere hacer). El código
+ *     “0” indica que no hay descuento. Los datos de la tienda deben estar almacenados en las cadenas de
+ *     caracteres cifTienda y nombreTienda (no se piden por teclado).
+ *
+ *     Ejemplo:
+ *      Por favor, introduzca el precio inicial del artículo en euros: 39.95
+ *      Introduzca el código de descuento (introduzca 0 si no hay descuento): AM68
+ *
+ *      MODAS LA GUAPILLA - CIF:1234567A
+ *      TIQUE DE COMPRA
+ *      Precio artículo: 39.95
+ *      Descuento: 10.00 (AM68)
+ *      Precio con descuento: 29.95
+ *      IVA: 6.29 (21%)
+ *      TOTAL: 36.24
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
