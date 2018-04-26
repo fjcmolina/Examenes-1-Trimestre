151eebdd06ebd96dc296e64966b765b075e806b0
fjcmolina <fjcmolina7@gmail.com>
2018-04-26 16:57:42 +0200

    Actualizando

---
+++ 2014_2015_t1c1b/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ *     Realiza un programa que pida 10 números enteros por teclado y que luego diga cuántos de esos
+ *     números son primos. Los primos repetidos también cuentan. El número 1, por convenio, no se
+ *     considera primo (el 0 tampoco es primo).
+ */
+package ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio2 {
+
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+  }
+  
+}
