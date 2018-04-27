8c061acef2c7cd760dffc52a8ecebe4bbc02eb8e
fjcmolina <fjcmolina7@gmail.com>
2018-04-27 09:52:46 +0200

    Actualizando

--- 2014_2015_t1c2b/Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ 2014_2015_t1c2b/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -18,13 +18,31 @@ public class Ejercicio2 {
     String[] palabras = new String[6];
 
     System.out.println("Introduce 6 palabras: ");
-    
+
     for (int i = 0; i < 6; i++) {
       palabras[i] = s.nextLine();
-      
+
     }
+
+    System.out.println("Array original: ");
+
     for (int i = 0; i < 6; i++) {
-    System.out.print(" " + palabras[i] + " ");
+      System.out.print(" " + palabras[i] + " ");
+    }
+
+    for (int i = 0; i < 500; i++) {
+      int x = (int) (Math.random() * 6);
+      int y = (int) (Math.random() * 6);
+      String temporal = palabras[x];
+      palabras[x] = palabras[y];
+      palabras[y] = temporal;
+    }
+    
+    System.out.println();
+    System.out.println("\nArray barajado: ");
+    
+    for (int i = 0; i < 6; i++) {
+      System.out.print(" " + palabras[i] + " ");
     }
   }
 
