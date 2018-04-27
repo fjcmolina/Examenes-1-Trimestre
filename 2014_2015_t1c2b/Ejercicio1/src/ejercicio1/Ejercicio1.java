14ac6456d4300196ecb0d2de11c8b8b73b2d8b2a
fjcmolina <fjcmolina7@gmail.com>
2018-04-27 09:17:58 +0200

    Actualizando

---
+++ 2014_2015_t1c2b/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,29 @@
+/*
+ *   Escribe un programa que genere el menú del día de un restaurante vegetariano. El programa debe pedir el día
+ *   de la semana, que será un número del uno al siete, y a continuación debe mostrar de forma aleatoria la
+ *   secuencia de platos en función de las siguientes condiciones: (a) De primer plato puede haber ensalada, pasta
+ *   o arroz (b) De segundo plato puede haber hamburguesas de soja, filetes de seitán o albóndigas de tofu que irán
+ *   acompañados con una guarnición que puede ser patatas bravas, verduras a la plancha o pimientos asados (c)
+ *   De postre puede haber flan, helado o arroz con leche (d) Cuando el primer plato es arroz, el postre no puede ser
+ *   arroz con leche (e) Los sábados y domingos no hay menú (se debe mostrar un mensaje que así lo indique).
+ *
+ *   Ejemplo:
+ *   Selecciona un día de la semana (un número del 1 al 7): 5
+ *   Menú del día: pasta de primero, albóndigas de soja con patatas bravas de segundo y arroz con leche de postre.
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  public static void main(String[] args) {
+    
+    SCanner s = new Scanner(System.in);
+  }
+  
+}
