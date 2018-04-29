3ac47873e775ca142a8002118ffcdb679a42df65
fjcmolina <fjcmolina7@gmail.com>
2018-04-29 13:06:04 +0200

    Actualizando

--- 2014_2015_t1recmayo/Ejercicio4/src/ejercicio4/Ejercicio4.java
+++ 2014_2015_t1recmayo/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -18,22 +18,91 @@ public class Ejercicio4 {
 
     Scanner s = new Scanner(System.in);
 
-    int[] numero = new int[10];
+    int[] n = new int[10];
+    int numero;
+    int[] par = new int[10];
+    int[] impar = new int[10];
+    int pares = 0;
+    int impares = 0;
 
     for (int i = 0; i < 10; i++) {
       System.out.print("Introduce un numero: ");
-      numero[i] = s.nextInt();
+      n[i] = s.nextInt();
+
+    }
+    System.out.println("Array original: ");
+
+    for (int i = 0; i < 10; i++) {
+
+      System.out.print(n[i] + " ");
+
+      if (n[i] % 2 == 0) {
+        pares++;
+      } else {
+        impares++;
+      }
     }
 
-    System.out.println("Array original: ");
+    int indicePar = 0;
+    int indiceImpar = 0;
+
     for (int i = 0; i < 10; i++) {
-      System.out.print(" " + numero[i] + " ");
+
+      if (n[i] % 2 == 0) {
+        par[indicePar] = n[i];
+        indicePar++;
+      } else {
+        impar[indiceImpar] = n[i];
+        indiceImpar++;
+      }
     }
-    
-    
-    System.out.println("Array modificado: ");
-    for (int i = 0; i < 10; i++) {
-      System.out.print(" " + numero[i] + " ");
+
+    int[] ordenado = new int[10];
+
+    if (pares > impares) {
+
+      indicePar = 0;
+      indiceImpar = 1;
+
+      for (int d = 0; d < impares; d++) {
+
+        ordenado[indicePar] = par[d];
+        ordenado[indiceImpar] = impar[d];
+        indicePar += 2;
+        indiceImpar += 2;
+      }
+
+      for (int d = impares; d < pares; d++) {
+
+        ordenado[indicePar] = par[d];
+        indicePar++;
+      }
+    } else {
+
+      indicePar = 0;
+      indiceImpar = 1;
+
+      for (int d = 0; d < pares; d++) {
+
+        ordenado[indicePar] = par[d];
+        ordenado[indiceImpar] = impar[d];
+        indicePar += 2;
+        indiceImpar += 2;
+      }
+
+      for (int d = pares; (d < impares) && (indiceImpar < 10); d++) {
+
+        ordenado[indiceImpar] = impar[d];
+        indiceImpar++;
+      }
+    }
+
+    System.out.println();
+    System.out.print("Array final: ");
+    System.out.println();
+
+    for (int r = 0; r < 10; r++) {
+      System.out.print(ordenado[r] + " ");
     }
   }
 
