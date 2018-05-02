bad759a26d388b4cdfc5d7246e2adce35798e57a
fjcmolina <fjcmolina7@gmail.com>
2018-05-02 11:57:00 +0200

    Actualizando

---
+++ prog_2015_2016__t1c1_turno2/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,42 @@
+/*
+ *     Calcula la nota de un trimestre de la asignatura Programación. El programa pedirá las dos notas que
+ *     ha sacado el alumno en los dos primeros controles. Si la media de los dos controles da un número
+ *     mayor o igual a 5, el alumno está aprobado y se mostrará la media. En caso de que la media sea un
+ *     número menor que 5, el alumno habrá tenido que hacer el examen de recuperación que se califica
+ *     como “apto” o “no apto”, por tanto se debe preguntar al usuario “¿Cuál ha sido el resultado de la
+ *     recuperación? (apto/no apto). Si el resultado de la recuperación es “apto”, la nota será un 5; en caso
+ *     contrario, se mantiene la nota media anterior.
+ *
+ *     Ejemplo 1:
+ *     Nota del primer control: 7
+ *     Nota del segundo control: 10
+ *     Tu nota de Programación es 8.5
+ *
+ *     Ejemplo 2:
+ *     Nota del primer control: 6
+ *     Nota del segundo control: 3
+ *     ¿Cuál ha sido el resultado de la recuperación? (apto/no apto): apto
+ *     Tu nota de Programación es 5
+ *
+ *     Ejemplo 3:
+ *     Nota del primer control: 6
+ *     Nota del segundo control: 3
+ *     ¿Cuál ha sido el resultado de la recuperación? (apto/no apto): no apto
+ *     Tu nota de Programación es 4.5
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
