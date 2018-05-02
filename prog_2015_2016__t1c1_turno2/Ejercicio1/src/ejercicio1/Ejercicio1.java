f78c095065fa6a5db721f0864f6f8d4038ae7bbf
fjcmolina <fjcmolina7@gmail.com>
2018-05-02 12:11:40 +0200

    Actualizando

--- prog_2015_2016__t1c1_turno2/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ prog_2015_2016__t1c1_turno2/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -26,17 +26,39 @@
  */
 package ejercicio1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Francis
  */
 public class Ejercicio1 {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Nota del primer control: ");
+    double nota1 = s.nextDouble();
+
+    System.out.print("Nota del segundo control: ");
+    double nota2 = s.nextDouble();
+
+    double media = ((nota1 + nota2) / 2);
+
+    if (media >= 5) {
+      System.out.print("Tu nota de Programación es " + media);
+    } else {
+      System.out.print("¿Cuál ha sido el resultado de la recuperación? (apto/no apto): ");
+      String recuperacion = s.next();
+
+      if (recuperacion.equals("apto")) {
+        System.out.print("Tu nota de Programacion es 5");
+      } else {
+        System.out.print("Tu nota de Programación es " + media);
+      }
+    }
+
   }
-  
+
 }
