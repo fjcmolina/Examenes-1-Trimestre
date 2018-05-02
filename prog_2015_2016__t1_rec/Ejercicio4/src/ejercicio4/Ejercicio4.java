65199b7d14224d23b24ac441b5379f17ae4a7fad
fjcmolina <fjcmolina7@gmail.com>
2018-05-02 14:02:33 +0200

    Actualizando

---
+++ prog_2015_2016__t1_rec/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,31 @@
+/*
+ *     Realiza un programa que gestione los votos de las próximas elecciones. La aplicación debe cumplir
+ *     los siguientes requisitos:
+ *
+ *     a) Los nombres de los partidos se deben guardar en un array llamada nombrePartido que se debe
+ *     inicializar al principio del programa con los siguientes nombres: Ciudadanos, Podemos, PP, PSOE,
+ *     Izquierda Unida y UpyD.
+ *     b) El programa debe pedir el número de votos que ha obtenido cada uno de los partidos. Estos
+ *     datos se deben guardar en un array llamado votacion.
+ *     c) El programa calculará la media de todas las votaciones y la mostrará por pantalla.
+ *     d) A continuación, el programa mostrará el mensaje: "Los partidos que han obtenido votos por
+ *     encima de la media son: " seguido de los nombres de los partidos cuyos votos han sido superiores
+ *     a la media.
+ *     e) A continuación, el programa mostrará el mensaje: "Los partidos que han obtenido votos por
+ *     debajo de la media son: " seguido de los nombres de los partidos cuyos votos han sido inferiores a
+ *     la media.
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio4 {
+
+  public static void main(String[] args) {
+    
+    
+  }
+  
+}
