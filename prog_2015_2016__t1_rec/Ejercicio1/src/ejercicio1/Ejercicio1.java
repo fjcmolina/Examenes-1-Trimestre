c9c0009262455f9fa4d414878c7976a8f43fd582
fjcmolina <fjcmolina7@gmail.com>
2018-05-02 13:31:11 +0200

    Actualizando

---
+++ prog_2015_2016__t1_rec/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ *       El nuevo partido político Izquierda Hacia la Izquierda nos ha pedido una aplicación que genere su
+ *       logotipo. El logotipo de este partido es la letra I inclinada hacia la izquierda. Se debe pedir por teclado
+ *       la altura del logotipo. La anchura siempre es de 4 asteriscos.
+ *
+ *       Ejemplo:
+ *       Introduzca la altura del logotipo: 5
+ *       ****
+ *        ****
+ *          ****
+ *            ****
+ *              ****
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduzca la altura del logotipo: ");
+    int altura = s.nextInt();
+
+  }
+
+}
