57c266223b3570051ef5697a7dc9d23520480ef2
fjcmolina <fjcmolina7@gmail.com>
2018-05-03 10:28:28 +0200

    Actualizando

---
+++ prog_2016_2017__t1c1b/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,41 @@
+/*
+ *     Una pastelería nos ha pedido realizar un programa que haga presupuestos de tartas. El programa
+ *     preguntará primero de qué sabor quiere el usuario la tarta: manzana, fresa o chocolate. La tarta de
+ *     manzana vale 18 euros y la de fresa 16. En caso de seleccionar la tarta de chocolate, el programa
+ *     debe preguntar además si el chocolate es negro o blanco; la primera opción vale 14 euros y la
+ *     segunda 15. Por último se pregunta si se añade nata y si se personaliza con un nombre; la nata suma
+ *     2’50 y la escritura del nombre 2’75.
+ *
+ *     Ejemplo 1:
+ *     Elija un sabor (manzana, fresa o chocolate): chocolate
+ *     ¿Qué tipo de chocolate quiere? (negro o blanco): negro
+ *     ¿Quiere nata? (si o no): si
+ *     ¿Quiere ponerle un nombre? (si o no): no
+ *     Tarta de chocolate negro: 14
+ *     Con nata: 2’50
+ *     Total: 16’50
+ *
+ *     Ejemplo 2:
+ *     Elija un sabor (manzana, fresa o chocolate): manzana
+ *     ¿Quiere nata? (si o no): no
+ *     ¿Quiere ponerle un nombre? (si o no): si
+ *     Tarta de manzana: 18
+ *     Con nombre: 2’75
+ *     Total: 20’75
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio1 {
+
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+  }
+  
+}
