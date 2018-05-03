b613cd9e5fa270573399c4e90c7b23ed073c4b4c
fjcmolina <fjcmolina7@gmail.com>
2018-05-03 20:00:47 +0200

    Actualizando

--- dam_prog_2017_2018__t1c1/Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ dam_prog_2017_2018__t1c1/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -2,7 +2,7 @@
  *     La tienda online BanderaDeEspaña.es vende banderas personalizadas de la máxima calidad y nos
  *     ha pedido hacer un configurador que calcule el precio según el alto y el ancho. El precio base de una
  *     bandera es de un céntimo de euro el centímetro cuadrado. Si la queremos con un escudo bordado, el
- *     precio se incrementa en 2.50 € independientemente del tamaño. Los gastos de envío son 3.25 €. El
+ *     precio se incrementa en 2.50 € independientemente del centimetros. Los gastos de envío son 3.25 €. El
  *     IVA ya está incluido en todas las tarifas.
  *
  *     Ejemplo:
@@ -35,23 +35,29 @@ public class Ejercicio2 {
     System.out.print("Ahora introduzca la anchura: ");
     int anchura = s.nextInt();
 
-    System.out.print(" ¿Quiere escudo bordado? (s/n):");
+    System.out.print("¿Quiere escudo bordado? (s/n):");
     String escudo = s.next();
 
     System.out.println();
-    int tamaño = altura * anchura;
+    int centimetros = altura * anchura;
+    double bandera = centimetros * 0.01;
 
-    System.out.print("Bandera de " + tamaño + " :");
+    System.out.println("Bandera de " + centimetros + "cm2 :" + bandera);
+
+    double precio = 0;
 
     if (escudo.equals("s")) {
-      System.out.print("Con escudo: ");
+      precio = 2.50;
+      System.out.println("Con escudo: " + precio);
     }
     if (escudo.equals("n")) {
-      System.out.println("Sin escudo: ");
+      precio = 0;
+      System.out.println("Sin escudo: " + precio);
     }
 
+    double total = bandera + precio + 3.5;
     System.out.println("Gastos de envio: 3.25 €");
-    System.out.println("Total: ");
+    System.out.println("Total: " + total);
   }
 
 }
