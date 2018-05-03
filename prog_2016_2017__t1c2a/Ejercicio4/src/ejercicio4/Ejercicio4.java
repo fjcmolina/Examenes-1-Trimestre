21dafce8f537ef7ac1033212d9f9ebc6b4eb8464
fjcmolina <fjcmolina7@gmail.com>
2018-05-03 13:14:23 +0200

    Actualizando

---
+++ prog_2016_2017__t1c2a/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,86 @@
+/*
+ *         Realiza un programa que pinte un sendero aleatorio. Los bordes se pintan con el carácter #. La
+ *         anchura del sendero siempre es la misma, los dos caracteres del borde más cuatro caracteres en
+ *         medio, en total 6 caracteres (incluyendo espacios). A cada metro, el sendero puede continuar recto,
+ *         girar un carácter a la izquierda o girar un carácter a la derecha, por supuesto de forma aleatoria. Por
+ *         cada metro de sendero – representado por una línea – puede que haya un obstáculo o puede que no,
+ *         la probabilidad es del 50%. La posición del obstáculo es aleatoria y puede estar entre el primer y el
+ *         cuarto carácter dentro de la línea. En caso de existir un obstáculo en un metro de sendero (en una
+ *         línea), puede ser una planta (carácter *) o una piedra (carácter O), la probabilidad de que salga uno u
+ *         otro es la misma. Recuerda que nunca habrá más de un obstáculo por metro de sendero, habrá uno o
+ *         ninguno.
+ *
+ *         Ejemplo 1:
+ *         Introduzca la longitud del sendero en metros: 7
+ *         # * #
+ *           #  O#
+ *           #   #
+ *         # *  #
+ *           #*  #
+ *          # O  #
+ *         #    #
+ *
+ *         Ejemplo 2:
+ *         Introduzca la longitud del sendero en metros: 5
+ *         #   #
+ *          # O #
+ *         #   #
+ *          # * #
+ *         #*  #
+ */
+package ejercicio4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio4 {
+
+  public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Vamos a crear un sendero serpenteante con obstaculos.");
+    System.out.print("De que longitud quiere que sea el sendero en metros: ");
+
+    int longitud = s.nextInt();
+    int espacios = 12; // Número de espacios inicial
+    int posObstaculo = -1; // Posición donde aparece el obstaculo
+
+    for (int i = 1; i <= longitud; i++) {
+
+      espacios += ((int) (Math.random() * 3) - 1); // Movemos el sendero
+
+      for (int j = 1; j <= espacios; j++) {
+        System.out.print(" "); // Pintamos las espacios
+      }
+
+      System.out.print("#"); // Pintamos el borde izquierdo del sendero
+
+      int obstaculo = (int) (Math.random() * 2);
+      if (obstaculo == 1) {
+        obstaculo = (int) (Math.random() * 2);
+        posObstaculo = (int) (Math.random() * 4);
+      }
+      for (int j = 0; j <= 3; j++) {
+        if (posObstaculo == j) {
+          switch (obstaculo) {
+            case 1:
+              System.out.print("O");
+              break; // Pintamos una piedra en el sendero
+            default:
+              System.out.print("*"); // Pintamos una planta en el sendero
+          }
+        } else {
+          System.out.print(" "); // Pintamos el sendero
+        }
+      }
+
+      posObstaculo = -1; // Reinicializamos la posición del obstaculo      
+      System.out.println("#"); // Pintamos el borde derecho del sendero
+    }
+  }
+
+}
