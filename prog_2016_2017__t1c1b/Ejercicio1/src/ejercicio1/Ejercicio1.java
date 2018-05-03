9f0647ef0d348d07632393f1b0abbfd1c8518f59
fjcmolina <fjcmolina7@gmail.com>
2018-05-03 10:48:31 +0200

    Actualizando

--- prog_2016_2017__t1c1b/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ prog_2016_2017__t1c1b/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -34,8 +34,69 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
   public static void main(String[] args) {
-    
+
     Scanner s = new Scanner(System.in);
+
+    System.out.print("Elija un sabor (manzana, fresa o chocolate): ");
+    String sabor = s.next();
+
+    double precio = 0;
+    String chocolate = "";
+    double precioNata = 0;
+    double precioNombre = 0;
+
+    if (sabor.equals("chocolate")) {
+
+      System.out.print(" ¿Qué tipo de chocolate quiere? (negro o blanco): ");
+      chocolate = s.next();
+
+      if (chocolate.equals("negro")) {
+        precio = 14;
+      }
+      if (chocolate.equals("blanco")) {
+        precio = 15;
+      }
+    }
+
+    if (sabor.equals("manzana")) {
+      precio = 18;
+    }
+
+    if (sabor.equals("fresa")) {
+      precio = 16;
+    }
+
+    System.out.print("¿Quiere nata? (si o no): ");
+    String nata = s.next();
+
+    if (nata.equals("si")) {
+      precioNata = 2.50;
+    }
+
+    System.out.print("¿Quiere ponerle un nombre? (si o no): ");
+    String nombre = s.next();
+
+    if (nombre.equals("si")) {
+      precioNombre = 2.75;
+    }
+
+    if (sabor.equals("chocolate")) {
+      System.out.println("Tarta de " + sabor + " " + chocolate + ": " + precio);
+    } else {
+      System.out.println("Tarta de " + sabor + ": " + precio);
+    }
+
+    if (nata.equals("si")) {
+      System.out.println("Con nata: " + precioNata);
+    }
+
+    if (nombre.equals("si")) {
+      System.out.println("Con nombre: " + precioNombre);
+    }
+    
+    double total = precio + precioNata + precioNombre;
+    
+    System.out.println("Total: " + total);
   }
-  
+
 }
