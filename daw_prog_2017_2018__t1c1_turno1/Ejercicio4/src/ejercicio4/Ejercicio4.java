ed779cb2acd2abfd96782f9928e519ebf4adb2d7
fjcmolina <fjcmolina7@gmail.com>
2018-05-04 10:35:18 +0200

    Actualizando

---
+++ daw_prog_2017_2018__t1c1_turno1/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,29 @@
+/*
+ *       Los números “donut” son los que tienen un 0 justo en la posición central, en caso de que el número de
+ *       cifras sea impar; o bien, los que tienen dos ceros en las posiciones centrales, en caso de que el
+ *       número de dígitos sea par. El resto de dígitos pueden ser ceros o no, no se tienen en cuenta. Por
+ *       ejemplo, los números 23037, 1008, 9060044, 0 y 8000 son “números donut”. Sin embargo 6758, 8903,
+ *       4200, 1234 y 7 no lo son. Realiza un programa que diga si un número introducido por teclado es un
+ *       “número donut” o no.
+ *
+ *       Ejemplo 1:
+ *       Introduzca un número entero positivo: 51803458
+ *       El 51803458 no es un número donut.
+ *
+ *       Ejemplo 2:
+ *       Introduzca un número entero positivo: 2960140
+ *       El 2960140 es un número donut.
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio4 {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
