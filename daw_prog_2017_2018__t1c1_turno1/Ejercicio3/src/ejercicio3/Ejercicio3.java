feafbf5cd0adc956bffeb23e6338f27018f33843
fjcmolina <fjcmolina7@gmail.com>
2018-05-04 10:26:12 +0200

    Actualizando

---
+++ daw_prog_2017_2018__t1c1_turno1/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,45 @@
+/*
+ *       Realiza un programa que pinte por pantalla un cuadrado a base de asteriscos con borde doble. El
+ *       usuario deberá introducir la longitud del lado del cuadrado que deberá ser mayor o igual a 5.
+ *
+ *       Ejemplo 1:
+ *       Introduzca la longitud del lado (5 como mínimo): 6
+ *       * * * * * *
+ *       * * * * * *
+ *       * *     * *
+ *       * *     * *
+ *       * * * * * *
+ *       * * * * * *
+ *
+ *       Ejemplo 2:
+ *       Introduzca la longitud del lado (5 como mínimo): 8
+ *       * * * * * * * *
+ *       * * * * * * * *
+ *       * *         * *
+ *       * *         * *
+ *       * *         * *
+ *       * *         * *
+ *       * * * * * * * *
+ *       * * * * * * * *
+ *
+ *       Ejemplo 3:
+ *       Introduzca la longitud del lado (5 como mínimo): 5
+ *       * * * * *
+ *       * * * * *
+ *       * *   * *
+ *       * * * * *
+ *       * * * * *
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author Francis
+ */
+public class Ejercicio3 {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
